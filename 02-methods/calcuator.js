f9f6cf4c8a8dc65f158c147f92bc11bd2f34385d
Gianluca Bragaglia <gianluca@Gianlucas-Mac-mini.local>
2018-03-05 10:01:15 +0100

    x

---
+++ 02-methods/calcuator.js
@@ -0,0 +1,19 @@
+
+/*Haz una calculadora. Un único programa al que le pasarás dos parámetros y el usuario podrá visualizar por consola la suma, resta, multiplicación y división entre ambos números. El resultado debería ser mostrado con 3 decimales como mucho (En caso de que hubieran). El programa debe contemplar y actuar correctamente en el caso de que el usuario introduzca cualquier cosa que no sean números.
+
+Si el usuario introduce un solo numero, deberá mostrar SOLO su raíz cuadrada, si vuelve a introducir los dos, volverá a mostrar las 4 operaciones de siempre.
+Los resultados deberían almacenarse dentro de una array y mostrarlos de una forma amigable al usuario.
+Hint_ => results = [num1 + num2 = resultSum, num1-num2 = resultRest ....]*/
+
+function calculator(num1, num2) {
+    var sum = num1 + num2;
+    var rest = num1 - num2;
+    var multi = num1 * num2;
+    var divide = num1 / num2;
+    var miavariabile = Math.SQRT2;
+    if(num1 == undefined) {
+        
+    }
+    console.log(miavariabile);
+    
+}
