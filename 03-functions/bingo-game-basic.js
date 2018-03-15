a7d17a0b7f24154b5e01068e481697dce59142ed
Gianluca Bragaglia <gianluca@Gianlucas-Mac-mini.local>
2018-03-15 11:46:36 +0100

    ok

--- 03-functions/bingo-game-basic.js
+++ 03-functions/bingo-game-basic.js
@@ -28,7 +28,7 @@ Sepáralo todo en funciones, englobado en una funcion global llamada bingo(), ta
 function bingo() {
 
     var playerName = prompt('Escribe tu nombre');
-    var card = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,];
+    var card = [1,2,3,4,5];
     console.log(card);
     
     
