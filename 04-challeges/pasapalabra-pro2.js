7361b27ff52a193be2a920af29803a43ab854edd
Gianluca Bragaglia <gianluca@Gianlucas-Mac-mini.local>
2018-03-28 01:05:26 +0200

    x

--- 04-challeges/pasapalabra-pro2.js
+++ 04-challeges/pasapalabra-pro2.js
@@ -18,76 +18,13 @@ El programa debe estar preparado para aceptar el input "END" para terminar el ju
 Prepara tu programa para que no repita siempre las mismas preguntas, por ejemplo, de la misma letra, se podrían hacer tres preguntas diferentes.
 Ejemplo de preguntas y respuestas: made by => www.github.com/misan7
 
-
-myVar = setTimeout(function, milliseconds);
-clearTimeout(myVar);   */
-
-function pasapalabraPro() {
-
-    var players = [];
-    var playerName;
-
-
-    this.newGame = function() {      // call function inside function HTML
-            playerName = document.querySelector('#nome').value;
-            var elem = document.getElementById("divName");     // remove div
-            elem.parentElement.removeChild(elem);
-            ask();
-            
-    }
-    return this;
-    
-    
-    function ask() {
-
-        var questions = [
-            { letter: "a", answer: "abducir", status: 0, question: ("CON LA A. Dicho de una supuesta criatura extraterrestre: Apoderarse de alguien") },
-            { letter: "b", answer: "bingo", status: 0, question: ("CON LA B. Juego que ha sacado de quicio a todos los 'Skylabers' en las sesiones de precurso") },
-            { letter: "c", answer: "churumbel", status: 0, question: ("CON LA C. Niño, crío, bebé") },
-            { letter: "d", answer: "diarrea", status: 0, question: ("CON LA D. Anormalidad en la función del aparato digestivo caracterizada por frecuentes evacuaciones y su consistencia líquida") },
-            { letter: "e", answer: "ectoplasma", status: 0, question: ("CON LA E. Gelatinoso y se encuentra debajo de la membrana plasmática. Los cazafantasmas medían su radiación") },
-            { letter: "f", answer: "facil", status: 0, question: ("CON LA F. Que no requiere gran esfuerzo, capacidad o dificultad") },
-            { letter: "g", answer: "galaxia", status: 0, question: ("CON LA G. Conjunto enorme de estrellas, polvo interestelar, gases y partículas") },
-            { letter: "h", answer: "harakiri", status: 0, question: ("CON LA H. Suicidio ritual japonés por desentrañamiento") },
-            { letter: "i", answer: "iglesia", status: 0, question: ("CON LA I. Templo cristiano") },
-            { letter: "j", answer: "jabali", status: 0, question: ("CON LA J. Variedad salvaje del cerdo que sale en la película 'El Rey León', de nombre Pumba") },
-            { letter: "k", answer: "kamikaze", status: 0, question: ("CON LA K. Persona que se juega la vida realizando una acción temeraria") },
-            { letter: "l", answer: "licantropo", status: 0, question: ("CON LA L. Hombre lobo") },
-            { letter: "m", answer: "misantropo", status: 0, question: ("CON LA M. Persona que huye del trato con otras personas o siente gran aversión hacia ellas") },
-            { letter: "n", answer: "necedad", status: 0, question: ("CON LA N. Demostración de poca inteligencia") },
-            { letter: "ñ", answer: "señal", status: 0, question: ("CONTIENE LA Ñ. Indicio que permite deducir algo de lo que no se tiene un conocimiento directo.") },
-            { letter: "o", answer: "orco", status: 0, question: ("CON LA O. Humanoide fantástico de apariencia terrible y bestial, piel de color verde creada por el escritor Tolkien") },
-            { letter: "p", answer: "protoss", status: 0, question: ("CON LA P. Raza ancestral tecnológicamente avanzada que se caracteriza por sus grandes poderes psíonicos del videojuego StarCraft") },
-            { letter: "q", answer: "queso", status: 0, question: ("CON LA Q. Producto obtenido por la maduración de la cuajada de la leche") },
-            { letter: "r", answer: "raton", status: 0, question: ("CON LA R. Roedor") },
-            { letter: "s", answer: "stackoverflow", status: 0, question: ("CON LA S. Comunidad salvadora de todo desarrollador informático") },
-            { letter: "t", answer: "terminator", status: 0, question: ("CON LA T. Película del director James Cameron que consolidó a Arnold Schwarzenegger como actor en 1984") },
-            { letter: "u", answer: "unamuno", status: 0, question: ("CON LA U. Escritor y filósofo español de la generación del 98 autor del libro 'Niebla' en 1914") },
-            { letter: "v", answer: "vikingos", status: 0, question: ("CON LA V. Nombre dado a los miembros de los pueblos nórdicos originarios de Escandinavia, famosos por sus incursiones y pillajes en Europa") },
-            { letter: "w", answer: "sandwich", status: 0, question: ("CONTIENE LA W. Emparedado hecho con dos rebanadas de pan entre las cuales se coloca jamón y queso") },
-            { letter: "x", answer: "botox", status: 0, question: ("CONTIENE LA X. Toxina bacteriana utilizada en cirujía estética") },
-            { letter: "y", answer: "peyote", status: 0, question: ("CONTIENE LA Y. Pequeño cáctus conocido por sus alcaloides psicoactivos utilizado de forma ritual y medicinal por indígenas americanos") },
-            { letter: "z", answer: "zen", status: 0, question: ("CON LA Z. Escuela de budismo que busca la experiencia de la sabiduría más allá del discurso racional") },
-        ];
-
-        var corrects = [];
-        var incorrects = 0;
-        var points = 0;
-        var count = 27;
-        var askFor;
-        
-            
-        //while(count > 0) {     
-
-            function questionLoop(i) {
+function questionLoop(i) {
                 var que = questions[i].question;
-                console.log(que);
                 document.getElementById("que").innerHTML = que;
                 var button = document.getElementById("btnValue");
                 button.onclick = function() {
                     askFor = document.querySelector('#queInput').value;
-                    clearTimeout(timerLoop);
-                    console.log(askFor);
+                    clearTimeout(timerLoop); 
                     if(questions[i].status == 0 || questions[i].status == 3) {
 
                         if(askFor == questions[i].answer) {
@@ -119,17 +56,125 @@ function pasapalabraPro() {
                             
                         }
                     }
-                       
+                     
                 i++;
-                if (i<=26) {var timerLoop = setTimeout(function(){questionLoop(i);},10000);}    
-                    
+                if (i<=26) {timerLoop = setTimeout(function(){questionLoop(i);},10000);}    
+  
                 }                 
-                
-                
-
-                
+               
             }
-            questionLoop(0);              
+            questionLoop(0); 
+
+myVar = setTimeout(function, milliseconds);
+clearTimeout(timerLoop);   */
+
+function pasapalabraPro() {
+
+    var players = [];
+    var playerName;
+    
+
+
+    this.newGame = function() {      // call function inside function HTML
+            playerName = document.querySelector('#nome').value;
+            var elem = document.getElementById("divName");     // remove div
+            elem.parentElement.removeChild(elem);
+            var resp = document.getElementById("resp");
+            resp.style.display = "block";
+            starT();
+            
+    }
+    return this;
+    
+    
+    function starT() {
+
+        var questions = [
+            { letter: "a", answer: "abducir", status: 0, question: ("CON LA A. Dicho de una supuesta criatura extraterrestre: Apoderarse de alguien") },
+            { letter: "b", answer: "bingo", status: 0, question: ("CON LA B. Juego que ha sacado de quicio a todos los 'Skylabers' en las sesiones de precurso") },
+            { letter: "c", answer: "churumbel", status: 0, question: ("CON LA C. Niño, crío, bebé") },
+            { letter: "d", answer: "diarrea", status: 0, question: ("CON LA D. Anormalidad en la función del aparato digestivo caracterizada por frecuentes evacuaciones y su consistencia líquida") },
+            { letter: "e", answer: "ectoplasma", status: 0, question: ("CON LA E. Gelatinoso y se encuentra debajo de la membrana plasmática. Los cazafantasmas medían su radiación") },
+            { letter: "f", answer: "facil", status: 0, question: ("CON LA F. Que no requiere gran esfuerzo, capacidad o dificultad") },
+            { letter: "g", answer: "galaxia", status: 0, question: ("CON LA G. Conjunto enorme de estrellas, polvo interestelar, gases y partículas") },
+            { letter: "h", answer: "harakiri", status: 0, question: ("CON LA H. Suicidio ritual japonés por desentrañamiento") },
+            { letter: "i", answer: "iglesia", status: 0, question: ("CON LA I. Templo cristiano") },
+            { letter: "j", answer: "jabali", status: 0, question: ("CON LA J. Variedad salvaje del cerdo que sale en la película 'El Rey León', de nombre Pumba") },
+            { letter: "k", answer: "kamikaze", status: 0, question: ("CON LA K. Persona que se juega la vida realizando una acción temeraria") },
+            { letter: "l", answer: "licantropo", status: 0, question: ("CON LA L. Hombre lobo") },
+            { letter: "m", answer: "misantropo", status: 0, question: ("CON LA M. Persona que huye del trato con otras personas o siente gran aversión hacia ellas") },
+            { letter: "n", answer: "necedad", status: 0, question: ("CON LA N. Demostración de poca inteligencia") },
+            { letter: "ñ", answer: "señal", status: 0, question: ("CONTIENE LA Ñ. Indicio que permite deducir algo de lo que no se tiene un conocimiento directo.") },
+            { letter: "o", answer: "orco", status: 0, question: ("CON LA O. Humanoide fantástico de apariencia terrible y bestial, piel de color verde creada por el escritor Tolkien") },
+            { letter: "p", answer: "protoss", status: 0, question: ("CON LA P. Raza ancestral tecnológicamente avanzada que se caracteriza por sus grandes poderes psíonicos del videojuego StarCraft") },
+            { letter: "q", answer: "queso", status: 0, question: ("CON LA Q. Producto obtenido por la maduración de la cuajada de la leche") },
+            { letter: "r", answer: "raton", status: 0, question: ("CON LA R. Roedor") },
+            { letter: "s", answer: "stackoverflow", status: 0, question: ("CON LA S. Comunidad salvadora de todo desarrollador informático") },
+            { letter: "t", answer: "terminator", status: 0, question: ("CON LA T. Película del director James Cameron que consolidó a Arnold Schwarzenegger como actor en 1984") },
+            { letter: "u", answer: "unamuno", status: 0, question: ("CON LA U. Escritor y filósofo español de la generación del 98 autor del libro 'Niebla' en 1914") },
+            { letter: "v", answer: "vikingos", status: 0, question: ("CON LA V. Nombre dado a los miembros de los pueblos nórdicos originarios de Escandinavia, famosos por sus incursiones y pillajes en Europa") },
+            { letter: "w", answer: "sandwich", status: 0, question: ("CONTIENE LA W. Emparedado hecho con dos rebanadas de pan entre las cuales se coloca jamón y queso") },
+            { letter: "x", answer: "botox", status: 0, question: ("CONTIENE LA X. Toxina bacteriana utilizada en cirujía estética") },
+            { letter: "y", answer: "peyote", status: 0, question: ("CONTIENE LA Y. Pequeño cáctus conocido por sus alcaloides psicoactivos utilizado de forma ritual y medicinal por indígenas americanos") },
+            { letter: "z", answer: "zen", status: 0, question: ("CON LA Z. Escuela de budismo que busca la experiencia de la sabiduría más allá del discurso racional") },
+        ];
+        var corrects = [];
+        var incorrects = 0;
+        var points = 0;
+        var count = 27;
+        var answ;
+        var timerLoop;
+        
+            
+        //while(count > 0) {     
+           
+
+            function letterFunc() {
+
+                var que = questions[i].question;
+                if(questions[i].status == 0 || questions[i].status == 3) {
+                    document.getElementById("que").innerHTML = que;
+                }
+
+                var button = document.getElementById("btnValue");
+                button.onclick = function() {
+                    answ = document.querySelector('#queInput').value;
+                    clearTimeout(timerLoop); 
+                    if(questions[i].status == 0 || questions[i].status == 3) {
+
+                        if(answ == questions[i].answer) {
+            
+                            points++;
+                            count--;
+                            questions[i].status = 1;
+                            corrects.push(questions[i].letter);
+                            alert('Correct, you have ' + points + ' Point!');
+                            questionLoop(0);
+                            
+            
+                        }else if(answ == 'pasapalabra') {
+            
+                            questions[i].status = 3;
+                            alert('Pasapalabra');
+                            questionLoop(0);
+                            
+                            
+                        
+                        }else if(answ !== 'pasapalabra' || answ !== questions[i].answer){
+                            
+                            questions[i].status = 2;
+                            incorrects++;
+                            count--;
+                            questionLoop(0);
+                            
+                        }
+                    }
+
+                }
+            }
+               
+            
+                        
            
         //}
 
@@ -138,7 +183,7 @@ function pasapalabraPro() {
             players.push({name: playerName, points: points});
             console.log(players);
             
-            console.log('Game Over!!' + playerName + ' has fallado ' + incorrects + ' letras, Has acertado esas letras: ' + corrects + ' - tienes ' + points + ' puntos' );
+            console.log('Game Over!!' + playerName + ' has fallado ' + incorrects + ' letras, Has acertado ' + points + ' letras: ' + corrects + ' - tienes ' + points + ' puntos' );
             var newPlayer = prompt('otro jugador? Y/N');
             switch(newPlayer) {
                 case 'y':
