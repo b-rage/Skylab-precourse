e6f5d7dcbdfab6fa1b93ac09506b532d26fbf2f2
Gianluca Bragaglia <gianluca@Gianlucas-Mac-mini.local>
2018-04-03 00:08:31 +0200

    x

---
+++ 06-pasapalabraproHTML/pasaplabra-pro-html.js
@@ -0,0 +1,18 @@
+
+/* 
+
+
+---- change color li id=idNum -----
+
+var idNum = 2;
+var highlight = function(el, colour) { 
+    el = document.getElementById(el) || false;
+    
+    if(el.style) {
+        el.style.color = colour;
+    }
+    
+    return el;
+};
+    
+    highlight(idNum, '#f0f'); */
